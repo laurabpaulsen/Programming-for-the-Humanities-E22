a83a360121a8e63f05a60c67139d2d57abbc96a7
knielbo <kln@cas.au.dk>
2022-08-28 12:31:30 +0200

    update dep

--- create_venv.sh
+++ create_venv.sh
@@ -2,6 +2,10 @@
 
 VENVNAME=ADA
 
+sudo apt update
+sudo apt install -y python3-venv
+sudo apt -y install zip unzip
+
 python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 
@@ -9,13 +13,10 @@ pip --version
 pip install --upgrade pip
 pip --version
 
-sudo apt-get -y install zip unzip
-
 # problems when installing from requirements.txt
-pip install ipython
-pip install jupyter
-
-python -m ipykernel install --user --name=$VENVNAME
+#pip install ipython
+#pip install jupyter
+#python -m ipykernel install --user --name=$VENVNAME
 
 test -f requirements.txt && pip install -r requirements.txt
 
